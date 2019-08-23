f2620973ea40bebf5b92508d85bc594144a67813
Vien Dinh <maxvien@gmail.com>
2019-08-23 15:31:36 +0700

    Update index.js

--- example/modules/index.js
+++ example/modules/index.js
@@ -4,4 +4,4 @@ const User = require('./user.module');
 const Post = require('./post.module');
 const Query = require('./query.module');
 
-module.exports = { typeDefs, resolvers } = allModules([User, Post, Query]);
+module.exports = { typeDefs, resolvers } = allModules([Query, User, Post]);
